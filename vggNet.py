f5da4d88e09f644d7634a24959e779e9397ff01a
“EchartsT <564749984@qq.com>
2019-04-29 11:39:29 +0800

    test

--- vggNet.py
+++ vggNet.py
@@ -89,7 +89,6 @@ cfg = {
     'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
-
 def vgg11(pretrained=False, **kwargs):
     """VGG 11-layer model (configuration "A")
     Args:
